ba1bf533d24fc158376cfcedd4b101fcda544e2e
Cheyenne T. Greatorex <ctg@magrathea.com>
2010-11-19 19:22:57 -0500

    Updated sample config file.
    Added sample controller and view.
    
    Updated most readme files.

---
+++ app/controllers/sample_controller.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class sample_controller extends BaseController {
+	/**
+	* This method is called before the requested action is executed.
+	*/
+	function beforeAction ($query) {
+	}
+
+
+	/**
+	* Define actions by writing methods of the same name. All action
+	* methods get one argument, the query string.
+	*/
+	function action ($query) {
+		$view = new View($this->name(), __FUNCTION__, $query);
+		$view->render_headfoot(TRUE);
+		$view->render();
+	}
+
+
+	/**
+	* This method is called after the requested action is executed.
+	*/
+	function afterAction ($query) {}
+}
