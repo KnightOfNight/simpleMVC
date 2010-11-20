9f135fef8f0a8199beb4c7fbbe3669937dadd538
Cheyenne T. Greatorex <ctg@magrathea.com>
2010-11-19 19:44:01 -0500

    Corrected class name.

--- app/models/SampleModel.class.php
+++ app/models/SampleModel.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class SamplesModel extends BaseModel {
+class SampleModel extends BaseModel {
 	function __construct () {
 		# If you need to override the name of the database table from the
 		# default (app_<lower case singular of class name>) then you should use
