2e02687310557ae93b0833a2a87adbfc5de89d7a
Cheyenne T. Greatorex <ctg@magrathea.com>
2011-06-25 17:12:53 -0400

    Improved auto loader.  Removed unused code.  Will now nicely Err::fatal if can't load.

--- lib/__autoload.php
+++ lib/__autoload.php
@@ -17,20 +17,14 @@
 * @param string name of the class to load
 */
 function __autoload ($class) {
-	$file = $class.".class.php";
 
-	require_once($file);
-#
-#	global $LIBDIR;
-#
-#	foreach (explode (":", $LIBDIR) as $dir) {
-#		$file = $dir.DS.$class.".class.php";
-#
-#		if (file_exists ($file) AND is_readable ($file)) {
-#			require_once ($file);
-#			return (TRUE);
-#		}
-#	}
-#
-#	return (FALSE);
+	if ( __class_exists($class) ) {
+		$file = $class.".class.php";
+		require_once($file);
+	} else {
+		Err::fatal("Autoload error: unable to load class '$class'.");
+	}
+
 }
+
+
