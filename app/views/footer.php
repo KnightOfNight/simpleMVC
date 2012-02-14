4c2d9b1eeeea5e0c0099bd0289f10929428b3833
Cheyenne T. Greatorex <cheyenne.greatorex@gmail.com>
2012-02-14 11:11:17 -0500

    Minor output change in sample footer view.

--- app/views/footer.php
+++ app/views/footer.php
@@ -2,6 +2,6 @@
 
 <br />
 <br />
-<div>Total time: <?php printf ("%.6f", microtime (TRUE) - $simpleMVC["start_time"]) ?></div>
+<div>Total script execution time: <?php printf ("%.6f", microtime (TRUE) - $simpleMVC["start_time"]) ?></div>
 </body>
 </html>
