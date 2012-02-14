c2c4e8898dea26f17ed2f9e708423cb2ee10292e
Cheyenne T. Greatorex <cheyenne.greatorex@gmail.com>
2012-02-14 11:20:19 -0500

    Updated hello view with page title.

--- app/views/welcome/hello.php
+++ app/views/welcome/hello.php
@@ -1,2 +1,4 @@
+<?php $this->fillslot('page_title', 'Welcome to simpleMVC!'); ?>
+
 Welcome to simpleMVC.  This application framework is aptly named, as it is<br />
 literally a simple model-view-controller web application framework.
