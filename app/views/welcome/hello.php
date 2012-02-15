bcefb797b54e105dd7d9029231ad9b6bc48c1a7c
Cheyenne T. Greatorex <cheyenne.greatorex@gmail.com>
2012-02-14 21:59:27 -0500

    updated welcome page

--- app/views/welcome/hello.php
+++ app/views/welcome/hello.php
@@ -1,8 +1,6 @@
 <?php $this->fillslot('page_title', 'Welcome to simpleMVC!'); ?>
 
-<div style="font-size:14pt">Welcome to <span style="font-weight:900">simpleMVC</span>!</div>
-
-<br />
+<h3>Welcome to simpleMVC!</h3>
 
 <div style="width:50%">
 This software does what it says on the tin.  It is a simple model-view-controller web application framework.  It is tested on Linux and requires Apache, MySQL, and PHP.
@@ -11,5 +9,12 @@ This software does what it says on the tin.  It is a simple model-view-controlle
 <div style="width:50%">
 Usage of this framework assumes beginning to intermediate skill in PHP programming; those proficient in that language should have no trouble using and extending its capabilities.
 </div>
+
+<h3>Getting Started</h3>
+<ol>
+<li>Models
+<li>Controllers
+<li>Views
+</ol>
 
 <br />
