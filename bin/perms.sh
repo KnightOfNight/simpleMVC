2f5f7d911888c0d7a89283bc254d5ffb8bc07172
Cheyenne T. Greatorex <cheyenne.greatorex@gmail.com>
2012-02-15 14:10:01 -0500

    moved perms setup to separate file so it can be run alone, updated setup.sh to use it

---
+++ bin/perms.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+chmod 755 .
+
+for dir in app mvc public tmp; do 
+	find $dir -type f -exec chmod a+r {} \; 2> /dev/null
+	find $dir -type d -exec chmod a+rx {} \; 2> /dev/null
+done
+
+find tmp -mindepth 1 -type d -exec chmod a+rwx {} \;
+
+for dir in doc setup ; do 
+	find $dir -type f -exec chmod 600 {} \;
+	find $dir -type d -exec chmod 700 {} \;
+done
+
+chmod 700 bin
+chmod 700 bin/*
+chmod 600 bin/.htaccess
