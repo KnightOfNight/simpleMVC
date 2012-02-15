b7712afe7a89e24ffd0c1173405791d7b0ea1986
Cheyenne T. Greatorex <cheyenne.greatorex@gmail.com>
2012-02-15 09:14:39 -0500

    updated versioning script

--- bin/version.sh
+++ bin/version.sh
@@ -20,11 +20,16 @@ fi
 
 
 echo "Setting version: $version ..."
+
 sed --in-place -e "s/@version .*/@version $version/" mvc/lib/*.php
-sed --in-place -e "s/PHP MVC [^\"][^\"]*/PHP MVC $version/" app/cfg/config.json
+
+sed --in-place -e "s/simpleMVC [^\"][^\"]*/simpleMVC $version/" setup/files/config.json
+
 echo "... source files updated."
 
 
 echo "Setting package: $package ..."
+
 sed --in-place -e "s/@package .*/@package $package/" mvc/lib/*.php
+
 echo "... source files updated."
