d25b3aa27350fc783c07b7b964e7e7e9920752ba
Cheyenne T. Greatorex <cheyenne.greatorex@gmail.com>
2012-02-14 21:58:47 -0500

    Updated sample

--- app/models/SampleModel.class.php
+++ app/models/SampleModel.class.php
@@ -1,12 +1,15 @@
 <?php
 
 class SampleModel extends BaseModel {
-	function __construct () {
-		# If you need to override the name of the database table from the
-		# default (app_<lower case singular of class name>) then you should use
-		# these lines.
-		#
-		# $this->table = "TABLENAME";
-		# $this->setup();
-	}
+
+	# NOTE: if you need to override the name of the database table from the
+	# default (app_<lower case singular of class name>) then you should use
+	# this...
+	#
+	# protected $table = 'TABLENAME';
+
+
+	# NOTE: if you need to extend the basic model functionaltiy, you can add
+	# static methods to this class.
+
 }
